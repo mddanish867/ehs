4657a65307445c7ddd2e023dd81158cef589abcd
mddanish867 <mddanish867@gmail.com>
2024-09-03 23:33:25 +0530

    changes the button style of login button in bottom header

--- src/Home/BottomHeader.jsx
+++ src/Home/BottomHeader.jsx
@@ -6,7 +6,7 @@ const BottomHeader = ({ toggleLoginForm }) => {
     <div className="fixed w-full bottom-0 inset-x-0 bg-white rounded-t-3xl py-10 px-4 md:hidden flex items-center justify-center space-x-4 shadow-red-400 shadow-2xl">
       <button
         onClick={toggleLoginForm}
-        className="text-blue-500 border border-blue-500 font-semibold p-2 w-full rounded-full focus:outline-none focus:ring-0 focus:ring-orange-600 focus:ring-opacity-50 transition duration-300 ease-in-out shadow-xl"
+        className="flex items-center space-x-2 lg:text-blue-500 lg:bg-white lg:hover:text-blue-700 bg-orange-600 text-white mb-2 rounded p-3 w-full "
       >
         Login
       </button>      
